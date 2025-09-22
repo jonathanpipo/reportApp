7e24804e9c0365ff351381b7e07109e0fd1b5a0d
jonathanpipo <jonathanpipo.ti@gmail.com>
2025-09-21 22:32:28 -0300

    add reportes migration

---
+++ database/migrations/2025_09_22_002032_create_reportes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reportes', function (Blueprint $table) {
+            $table->id(); // cria o campo 'id' auto-increment
+            $table->string('categoria', 100);
+            $table->string('subcategoria', 100);
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->decimal('precisao', 10, 2)->nullable();
+            $table->text('descricao')->nullable();
+            $table->timestamp('data_registro')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reportes');
+    }
+};
